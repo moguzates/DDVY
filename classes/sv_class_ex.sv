69863d047bbd118723583daf26b029d2426bad75
moguzates <mustafaoguzates@gmail.com>
2025-02-28 14:52:58 +0300

    added new class

---
+++ classes/sv_class_ex.sv
@@ -0,0 +1,28 @@
+class sv_class;
+  //class properties
+  int x;
+
+  //method-1
+  task set(int i);
+    x = i;
+  endtask
+
+  //method-2
+  function int get();
+    return x;
+  endfunction
+endclass
+
+module sv_class_ex;
+ sv_class class_1; //Creating Handle
+
+  initial begin
+    automatic sv_class class_2 = new(); //Creating handle and Object  //automatic used cause of dsim
+    class_1 = new(); //Creating Object for the Handle
+    //Accessing Class methods
+    class_1.set(10);
+    class_2.set(20);
+    $display("\tclass_1 :: Value of x = %0d",class_1.get());
+    $display("\tclass_2 :: Value of x = %0d",class_2.get());
+  end
+endmodule
